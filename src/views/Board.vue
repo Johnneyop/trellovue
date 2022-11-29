b25bfcfaf43ca7135e2aa97e5d5bb8f8d3505e49
johnneyop <johnneyop@gmail.com>
2022-11-29 13:14:28 -0600

    Add task: add and drop and edit api

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="board">
     <div class="flex flex-row items-start">
-      <div 
-        class="column" 
+      <div class="column"
         v-for="(column, $columnIndex) of board.columns"
         :key="$columnIndex"
+        @drop="moveTask($event, column.tasks)"
+        @dragover.prevent
+        @dragenter.prevent
       >
         <div class="flex items-center mb-2 font-bold">
           {{ column.name }}
         </div>
         <div class="list-reset">
-          <div 
-            class="task"
+          <div
             v-for="(task, $taskIndex) of column.tasks"
             :key="$taskIndex"
+            class="task"
+            draggable
+            @dragstart="pickupTask($event, $taskIndex, $columnIndex)"
             @click="goToTask(task)"
-            >
+          >
             <span class="w-full flex-no-shrink font-bold">
               {{ task.name }}
             </span>
@@ -26,10 +30,16 @@
               {{ task.description }}
             </p>
           </div>
+
+          <input
+            type="text"
+            class="block p-2 w-full bg-transparent"
+            placeholder="+ Enter new task"
+            @keyup.enter="createTask($event, column.tasks)"
+          />
         </div>
       </div>
     </div>
-
     <div 
       class="task-bg"
       v-if="isTaskOpen"
@@ -52,15 +62,37 @@ export default {
   },
   methods: {
     goToTask (task) {
-      this.$router.push({ name: 'task', params: { id:task.id } })
+      this.$router.push({ name: 'task', params: { id: task.id } })
     },
     close () {
       this.$router.push({ name: 'board' })
+    },
+    createTask (e, tasks) {
+      this.$store.commit('CREATE_TASK', {
+        tasks,
+        name: e.target.value
+      })
+      e.target.value = ''
+    },
+    pickupTask (e, taskIndex, fromColumnIndex) {
+      e.dataTransfer.effectAllowed = 'move'
+      e.dataTransfer.dropEffect = 'move'
+      e.dataTransfer.setData('task-index', taskIndex)
+      e.dataTransfer.setData('from-column-index', fromColumnIndex)
+    },
+    moveTask (e, toTasks) {
+      const fromColumnIndex = e.dataTransfer.getData('from-column-index')
+      const fromTasks = this.board.columns[fromColumnIndex].tasks
+      const taskIndex = e.dataTransfer.getData('task-index')
+      this.$store.commit('MOVE_TASK', {
+        fromTasks,
+        toTasks,
+        taskIndex
+      })
     }
   }
 }
 </script>
-
 
 <style lang="css">
 .task {
